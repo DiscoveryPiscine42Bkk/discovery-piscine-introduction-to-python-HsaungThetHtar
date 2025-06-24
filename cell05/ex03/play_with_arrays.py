01d0abe1695cc930c2deda4e9781b7d0f8ade4c3
HTH <106326336+HsaungThetHtar@users.noreply.github.com>
2025-06-24 20:09:53 +0700

    Create play_with_arrays.py

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,12 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+new_array = []
+for value in original_array:
+    if value > 5:
+        new_array.append(value + 2)
+
+new_set = set(new_array)
+
+print(original_array)
+print(new_set)
+
