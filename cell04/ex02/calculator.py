2e62fb0781e0d2a877090ae8d767d5065bc918a4
HTH <hsaungthethtar03@gmail.com>
2025-06-25 17:09:07 +0700

    updated

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 a = int(input("Give me the first number: "))
 b = int(input("Give me the second number: "))
 print("Thank you!")
---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def add_one(parameter):
+    parameter = parameter + 1
+    return parameter
+
+my_variable = 5
+print(my_variable)
+
+add_one(my_variable)
+
+print(my_variable)
