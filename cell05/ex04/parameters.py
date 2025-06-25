6113212e0865e9e5e72fb05982c2363e932a0da0
HTH <hsaungthethtar03@gmail.com>
2025-06-25 13:11:22 +0700

    updated

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,7 +1,3 @@
 import sys
-def para(*args):
-    print(f"Number of parameters: {len(args)}.")
 
-para()
-para("initiation")
-para("this","is", "crazy", "there's", "everywhere!")
+print(len(sys.argv)-1)
